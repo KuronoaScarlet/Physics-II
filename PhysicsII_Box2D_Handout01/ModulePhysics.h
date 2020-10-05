0591d9cee2a7391bab8b51cc3c0cd3ac68d3e593
Ignasi Pardo Carbó <60847464+KuronoaScarlet@users.noreply.github.com>
2020-10-05 18:52:38 +0200

    Intento fallido

---
+++ PhysicsII_Box2D_Handout01/ModulePhysics.h
@@ -0,0 +1,23 @@
+#pragma once
+#include "Module.h"
+#include "Globals.h"
+#include "Box2D.h"
+
+#pragma comment(lib, "Box2D/libx86/Debug/Box2D.lib")
+
+class ModulePhysics : public Module
+{
+public:
+	ModulePhysics(Application* app, bool start_enabled = true);
+	~ModulePhysics();
+
+	bool Start();
+	update_status PreUpdate();
+	update_status PostUpdate();
+	bool CleanUp();
+
+private:
+	b2Vec2 gravity = b2Vec2(0.0f, -10.0f);
+	b2World* world = nullptr;
+	bool debug;
+};
